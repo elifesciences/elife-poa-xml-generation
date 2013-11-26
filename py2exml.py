8400c703e407de27da7b23acd812bd0aac3d6e1e
ian <ian@orac.local>
2013-11-26 17:16:15 +0000

    initial script that converst from a dict to XML - taken from the internet

---
+++ py2exml.py
@@ -0,0 +1,57 @@
+import xml
+from xml.dom.minidom import Document
+import copy
+from collections import namedtuple
+
+class dict2xml(object):
+    doc     = Document()
+
+    def __init__(self, structure):
+        if len(structure) == 1:
+            rootName    = str(structure.keys()[0])
+            self.root   = self.doc.createElement(rootName)
+
+            self.doc.appendChild(self.root)
+            self.build(self.root, structure[rootName])
+
+    def build(self, father, structure):
+        if type(structure) == dict:
+            for k in structure:
+                tag = self.doc.createElement(k)
+                father.appendChild(tag)
+                self.build(tag, structure[k])
+
+        elif type(structure) == list:
+            grandFather = father.parentNode
+            tagName     = father.tagName
+            # grandFather.removeChild(father)
+            for l in structure:
+                tag = self.doc.createElement(tagName.rstrip('s'))
+                self.build(tag, l)
+                father.appendChild(tag)
+
+        else:
+            data    = str(structure)
+            tag     = self.doc.createTextNode(data)
+            father.appendChild(tag)
+
+    def display(self):
+        print self.doc.toprettyxml(indent="  ")
+
+if __name__ == '__main__':
+    
+    example = {'sibbling':{'couple':{'mother':'mom','father':'dad','children':[{'child':'foo'},
+                                                          {'child':'bar'}]}}}
+    xml = dict2xml(example)
+    xml.display()
+
+
+article = {
+            "title" : ""
+            }
+
+Article = namedtuple('Title', 'title'                    )
+sub = Article("The Title")
+
+print sub.title 
+ 
