42c5eeac0c2a88fa00908b112396ae39bc86977c
Ian Mulvany <ian@mulvany.net>
2014-03-06 15:06:52 +0000

    no longer copying files with the dummy postfix

--- quick_copy_xml.sh
+++ quick_copy_xml.sh
@@ -1,9 +1,9 @@
-cp ./generated_xml_output/elife_poa_e00003dummy.xml ./unpacked_renamed_ejp_files/
-cp ./generated_xml_output/elife_poa_e00005dummy.xml ./unpacked_renamed_ejp_files/
-cp ./generated_xml_output/elife_poa_e00007dummy.xml ./unpacked_renamed_ejp_files/
-cp ./generated_xml_output/elife_poa_e00012dummy.xml ./unpacked_renamed_ejp_files/ 
-cp ./generated_xml_output/elife_poa_e00013dummy.xml ./unpacked_renamed_ejp_files/
-cp ./generated_xml_output/elife_poa_e00031dummy.xml ./unpacked_renamed_ejp_files/
-cp ./generated_xml_output/elife_poa_e00036dummy.xml ./unpacked_renamed_ejp_files/
-cp ./generated_xml_output/elife_poa_e00047dummy.xml ./unpacked_renamed_ejp_files/
+cp ./generated_xml_output/elife_poa_e00003.xml ./unpacked_renamed_ejp_files/
+cp ./generated_xml_output/elife_poa_e00005.xml ./unpacked_renamed_ejp_files/
+cp ./generated_xml_output/elife_poa_e00007.xml ./unpacked_renamed_ejp_files/
+cp ./generated_xml_output/elife_poa_e00012.xml ./unpacked_renamed_ejp_files/ 
+cp ./generated_xml_output/elife_poa_e00013.xml ./unpacked_renamed_ejp_files/
+cp ./generated_xml_output/elife_poa_e00031.xml ./unpacked_renamed_ejp_files/
+cp ./generated_xml_output/elife_poa_e00036.xml ./unpacked_renamed_ejp_files/
+cp ./generated_xml_output/elife_poa_e00047.xml ./unpacked_renamed_ejp_files/
 
