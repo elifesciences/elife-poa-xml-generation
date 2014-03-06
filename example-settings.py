dce7bdfaa0600a98b7ad91c4168e42080d3ee8ab
Ian Mulvany <ian@mulvany.net>
2014-03-06 14:03:34 +0000

    added placholder for ftp access
    added info on where files for the workflow should be located.

--- example-settings.py
+++ example-settings.py
@@ -1,13 +1,13 @@
 
-# xls reading settings 
+# xls reading settings
 ROWS_WITH_COLNAMES = 3
 DATA_START_ROW = 4
 
-XLS_PATH = "/Users/ian/Dropbox/code/private-code/poa-xls-files/csv-input-v1.06/" 
+XLS_PATH = "/Users/ian/Dropbox/code/private-code/poa-xls-files/csv-input-v1.06/"
 
 
-        
-         
+
+
 XLS_FILES = 	{"authors" : "ejp_query_tool_query_id_177_POA_Author_2014_02_12_eLife.csv",
 				 "license" : "ejp_query_tool_query_id_178_POA_License_2014_02_12_eLife.csv",
 				 "manuscript" : "ejp_query_tool_query_id_176_POA_Manuscript_2014_02_12_eLife.csv",
@@ -26,8 +26,8 @@ XLS_COLUMN_HEADINGS = {"author_position" : "poa_a_seq",
 					"editor_last_name" : "poa_m_me_last_nm",
 					"editor_first_name" : "poa_m_me_first_nm",
 					"editor_middle_name" : "poa_m_me_middle_nm",
-					"editor_institution" : "poa_m_me_organization", 
-					"editor_department" : "poa_m_me_department", 
+					"editor_institution" : "poa_m_me_organization",
+					"editor_department" : "poa_m_me_department",
 					"editor_country" : "poa_m_me_country",
 					"ethics" : "poa_m_ethics_note",
 					"author_id" : "poa_a_id",
@@ -39,18 +39,21 @@ XLS_COLUMN_HEADINGS = {"author_position" : "poa_a_seq",
 					"author_middle_name" : "poa_a_middle_nm",
 					"author_institution" : "poa_a_organization",
 					"author_department" : "poa_a_department",
-					"author_city" : "poa_a_city", 
+					"author_city" : "poa_a_city",
 					"author_country" : "poa_a_country",
 					"author_state" : "poa_a_state",
 					"author_conflict" : "poa_a_cmp",
 					"organisms" : "poa_ro_researchorganism"
 				}
 
-# xml writing settings 
+# xml writing settings
 TARGET_OUTPUT_DIR = "generated_xml_output/"
 
 STAGING_TO_HW_DIR = "unpacked_renamed_ejp_files/"
 FTP_TO_HW_DIR = "ftp-to-hw"
 MADE_FTP_READY = "made_ftp_ready_on"
-EJP_INPUT_DIR = "sample-zip-from-ejp" 
+EJP_INPUT_DIR = "sample-zip-from-ejp"
 
+FTP_URI = ""
+FTP_USERNAME = ""
+FTP_PASSWORD = ""
