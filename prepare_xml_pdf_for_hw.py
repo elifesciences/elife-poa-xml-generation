24a7d330213b7d83289d173c6d7f1fd4ffd41a85
Ian Mulvany <ian@mulvany.net>
2014-03-06 15:54:02 +0000

    created a main() function
    added the folloing functions to make the main function tiduer
    
    def set_datestamp():
    	a = arrow.utcnow()
    	date_stamp = str(a.datetime.year) + str(a.datetime.month).zfill(2) + str(a.datetime.day).zfill(2)
    	return date_stamp
    
    def set_xml_pdf_zip_name():
    	date_stamp = set_datestamp()
    	xml_pdf_zip = "elife_poa_" + date_stamp + ".zip"
    	return xml_pdf_zip
    
    def set_made_ftp_ready_dir():
    	date_stamp = set_datestamp()
    	made_ftp_ready = settings.MADE_FTP_READY
    	made_ftp_ready_dir = made_ftp_ready + "/" + date_stamp
    	if not os.path.exists(made_ftp_ready_dir):
    		os.makedirs(made_ftp_ready_dir)
    	return made_ftp_ready_dir
    
    Added logging, both local and global, set logging to "INFO" during debug

--- prepare_xml_pdf_for_hw.py
+++ prepare_xml_pdf_for_hw.py
@@ -1,16 +1,17 @@
 import glob
 import settings as settings
-import zipfile 
+import zipfile
 import arrow
-import os 
+import os
 import shutil
+import logging
 
 """
 look in unpacked_renamed_ejp_files
 
-look for all matching pdf and xml files 
+look for all matching pdf and xml files
 	elife_poa_e000213.xml
-	elife_poa_e000213.pdf 
+	elife_poa_e000213.pdf
 
 If there is an xml or pdf file that is not matched, log an error
 
@@ -20,13 +21,31 @@ for the day of delivery take these files and put them into a zip file named
 put that zip file into `ftp-to-hw`
 
 move processed pdf and xml files into
-	made_ftp_ready_on/YYYMMDD  
+	made_ftp_ready_on/YYYMMDD
 
 
 GOTCHAS
-When run multiple times it may possibly corrupt exising zip files, worthy of investigation. 
+When run multiple times it may possibly corrupt exising zip files, worthy of investigation.
 
 """
+
+## Setup logging
+# local logger
+logger = logging.getLogger('prepPdfXMLforFTP')
+hdlr = logging.FileHandler('prepPdfXMLforFTP.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
+# global logger
+workflow_logger = logging.getLogger('ejp_to_hw_workflow')
+hdlr = logging.FileHandler('ejp_to_hw_workflow.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+workflow_logger.addHandler(hdlr)
+workflow_logger.setLevel(logging.INFO)
+
 
 def zip(src, dst):
     zf = zipfile.ZipFile("%s.zip" % (dst), "w")
@@ -35,19 +54,17 @@ def zip(src, dst):
         for filename in files:
             absname = os.path.abspath(os.path.join(dirname, filename))
             arcname = absname[len(abs_src) + 1:]
-            print 'zipping %s as %s' % (os.path.join(dirname, filename),
-                                        arcname)
+            logger.info('zipping %s as %s' % (os.path.join(dirname, filename),
+                                        arcname))
             zf.write(absname, arcname)
     zf.close()
 
-# zip("src", "dst")
-
 def check_matching_files_exist(pdf_file_articles_numbers, xml_file_articles_numbers):
 	for file in pdf_file_articles_numbers:
-		if file not in xml_file_articles_numbers: print file, " has no xml match"
+		if file not in xml_file_articles_numbers: logger.warning(str(file) + " has no xml match")
 
 	for file in xml_file_articles_numbers:
-		if file not in pdf_file_articles_numbers: print file, " has no pdf match"
+		if file not in pdf_file_articles_numbers: logger.warning(str(file) + " has no pdf match")
 
 def zip_matching_files(pdf_file_articles_numbers, xml_file_articles_numbers, zf):
 	for file in pdf_file_articles_numbers:
@@ -57,7 +74,6 @@ def zip_matching_files(pdf_file_articles_numbers, xml_file_articles_numbers, zf)
 			zf.write(absname, arcname)
 			absname = file + ".xml"
 			arcname = absname.split("/")[1]
-			print absname, arcname
 			zf.write(absname, arcname)
 
 def move_zipfile_to_hw_staging(xml_pdf_zip, ftp_to_hw):
@@ -68,41 +84,49 @@ def move_processed_files(pdf_file_articles_numbers, xml_file_articles_numbers, s
 		if file in xml_file_articles_numbers:
 			absname = file + ".pdf"
 			arcname = absname.split("/")[1]
-			print absname
-			print arcname
 			shutil.move(sourcedir + "/" + arcname, made_ftp_ready + "/" + arcname)
 			absname = file + ".xml"
-			arcname = absname.split("/")[1]			
+			arcname = absname.split("/")[1]
 			shutil.move(sourcedir + "/" + arcname, made_ftp_ready + "/" + arcname)
 
-sourcedir = settings.STAGING_TO_HW_DIR
-ftp_to_hw = settings.FTP_TO_HW_DIR
+def set_datestamp():
+	a = arrow.utcnow()
+	date_stamp = str(a.datetime.year) + str(a.datetime.month).zfill(2) + str(a.datetime.day).zfill(2)
+	return date_stamp
 
-pdf_files = glob.glob(sourcedir + "/*.pdf")
-xml_files = glob.glob(sourcedir + "/*.xml")
+def set_xml_pdf_zip_name():
+	date_stamp = set_datestamp()
+	xml_pdf_zip = "elife_poa_" + date_stamp + ".zip"
+	return xml_pdf_zip
 
-pdf_file_articles_numbers = []
-xml_file_articles_numbers = []
-for f in pdf_files: pdf_file_articles_numbers.append(f.split(".pdf")[0])
-for f in xml_files: xml_file_articles_numbers.append(f.split(".xml")[0])
+def set_made_ftp_ready_dir():
+	date_stamp = set_datestamp()
+	made_ftp_ready = settings.MADE_FTP_READY
+	made_ftp_ready_dir = made_ftp_ready + "/" + date_stamp
+	if not os.path.exists(made_ftp_ready_dir):
+		os.makedirs(made_ftp_ready_dir)
+	return made_ftp_ready_dir
 
-a = arrow.utcnow() 
-print str(a.datetime.year)
-print str(a.datetime.month).zfill(2)
-print str(a.datetime.day).zfill(2)
+def prepare_pdf_xml_for_ftp():
+	sourcedir = settings.STAGING_TO_HW_DIR
+	ftp_to_hw = settings.FTP_TO_HW_DIR
+	pdf_files = glob.glob(sourcedir + "/*.pdf")
+	xml_files = glob.glob(sourcedir + "/*.xml")
 
-date_stamp = str(a.datetime.year) + str(a.datetime.month).zfill(2) + str(a.datetime.day).zfill(2)
+	pdf_file_articles_numbers = []
+	xml_file_articles_numbers = []
+	for f in pdf_files: pdf_file_articles_numbers.append(f.split(".pdf")[0])
+	for f in xml_files: xml_file_articles_numbers.append(f.split(".xml")[0])
 
-xml_pdf_zip = "elife_poa_" + date_stamp + ".zip"
-made_ftp_ready = settings.MADE_FTP_READY
+	made_ftp_ready_dir = set_made_ftp_ready_dir()
+	xml_pdf_zip = set_xml_pdf_zip_name()
+	zf = zipfile.ZipFile(xml_pdf_zip, "w")
 
-made_ftp_ready_dir = made_ftp_ready + "/" + date_stamp
-if not os.path.exists(made_ftp_ready_dir):
-    os.makedirs(made_ftp_ready_dir)
+	check_matching_files_exist(pdf_file_articles_numbers, xml_file_articles_numbers)
+	zip_matching_files(pdf_file_articles_numbers, xml_file_articles_numbers, zf)
+	move_zipfile_to_hw_staging(xml_pdf_zip, ftp_to_hw)
+	move_processed_files(pdf_file_articles_numbers, xml_file_articles_numbers, sourcedir, made_ftp_ready_dir)
 
-zf = zipfile.ZipFile(xml_pdf_zip, "w")
-check_matching_files_exist(pdf_file_articles_numbers, xml_file_articles_numbers)
-zip_matching_files(pdf_file_articles_numbers, xml_file_articles_numbers, zf)
-move_zipfile_to_hw_staging(xml_pdf_zip, ftp_to_hw)
-move_processed_files(pdf_file_articles_numbers, xml_file_articles_numbers, sourcedir, made_ftp_ready_dir)
-
+if __name__ == "__main__":
+	prepare_pdf_xml_for_ftp()
+	workflow_logger.info("pdf and xml files prepared in readyness to ftp")
