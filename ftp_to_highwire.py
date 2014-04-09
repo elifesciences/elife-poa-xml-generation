1da31c2f7da7a1dfb854c7c9793f266ea241ff91
Graham Nott <gnott@starglobal.ca>
2014-04-08 18:45:32 -0700

    ftp_to_highwire.py tweaks and clean-up.

--- ftp_to_highwire.py
+++ ftp_to_highwire.py
@@ -8,7 +8,7 @@ Will deliver to the named HW ftp bucket.
 
 
 
-## Gotchas
+## Gotchas
 
 I noted that on one run with a large number of PDFs the upload speed
 was very slow, might need to be investigated
@@ -24,13 +24,12 @@ ftpuri = settings.FTP_URI
 ftpusername = settings.FTP_USERNAME
 ftppassword = settings.FTP_PASSWORD
 
-zipfiles = glob.glob(source_dir + "/*.zip")
 
 
 def upload(ftp, file):
 	ext = os.path.splitext(file)[1]
 	print file
-	uploadname = file.split("/")[1]
+	uploadname = file.split(os.sep)[-1]
 	if ext in (".txt", ".htm", ".html"):
 		ftp.storlines("STOR " + file, open(file))
 	else:
@@ -38,7 +37,14 @@ def upload(ftp, file):
 		ftp.storbinary("STOR " + uploadname, open(file, "rb"), 1024)
 		print "uploaded " + uploadname
 
-for zipfile in zipfiles:
-	ftp = FTP(ftpuri, ftpusername, ftppassword)
-	upload(ftp, zipfile)
-	ftp.quit()
+def ftp_to_endpoint(zipfiles):
+	for zipfile in zipfiles:
+		ftp = FTP(ftpuri, ftpusername, ftppassword)
+		upload(ftp, zipfile)
+		ftp.quit()
+	
+if __name__ == "__main__":
+	zipfiles = glob.glob(source_dir + "/*.zip")
+	ftp_to_endpoint(zipfiles)
+	workflow_logger.info("files uploaded to endpoint using ftp")
+
