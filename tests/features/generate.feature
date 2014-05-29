2b1477d4fc474a9df0d928c6c74e5ccb8b9044ec
Graham Nott <gnott@starglobal.ca>
2014-05-28 17:49:11 -0700

    Lettuce tests reading CSV files.

--- tests/features/generate.feature
+++ tests/features/generate.feature
@@ -23,6 +23,17 @@ Feature: Generate POA XML
   Examples:
     | property                                    | value
     | LESS_THAN_ESCAPE_SEQUENCE                   | foo
+    | XLS_PATH                                    | test_data/
+
+  Scenario: Override json settings values
+    Given I have settings
+    When I set json settings <property> to <value>
+    And I read settings <property>
+    Then I have the json value <value>
+    
+  Examples:
+    | property                                    | value
+    | XLS_FILES                                   | {"authors" : "poa_author.csv", "license" : "poa_license.csv", "manuscript" : "poa_manuscript.csv", "received" : "poa_received.csv", "subjects" : "poa_subject_area.csv", "organisms": "poa_research_organism.csv"}
 
   Scenario: Test entity to unicode conversion
     Given I have the string <string>
@@ -47,3 +58,23 @@ Feature: Generate POA XML
     | GTGT                                        | >
     | LTLTGTGT                                    | <>
     | LTLTiGTGTeyeLTLT/iGTGT                      | <i>eye</i>
+    
+  Scenario: Parse CSV files
+    Given I have settings
+    And I reload settings
+    And I set settings XLS_PATH to test_data/
+    And I set json settings XLS_FILES to {"authors" : "poa_author.csv", "license" : "poa_license.csv", "manuscript" : "poa_manuscript.csv", "received" : "poa_received.csv", "subjects" : "poa_subject_area.csv", "organisms": "poa_research_organism.csv"}
+    And I reload XML generation libraries
+    And I have article_id <article_id>
+    And I have author_id <author_id>
+    When I set attribute to parseCSVFiles method <method_name>
+    Then I have attribute <attribute>
+    
+  Examples:
+    | article_id    | author_id  | method_name             | attribute
+    | 00003         |            | get_title               | A novel role for lipid droplets in the organismal antibacterial response
+    | 00003         | 1258       | get_author_first_name   | Preetha
+    | 00012         |            | get_license             | 1
+    | 00007         |            | get_organisms           | Other
+    | 00003         |            | get_organisms           | B. subtilis,D. melanogaster,E. coli,Mouse
+    | 00007         |            | get_subjects            | Genomics and evolutionary biology,Plant biology
--- tests/features/generate_steps.py
+++ tests/features/generate_steps.py
@@ -1,5 +1,7 @@
 from lettuce import *
 from generatePoaXml import *
+import parseCSVFiles
+import json
 
 @step(u'I have the string (.*)')
 def i_have_the_string_string(step, string):
@@ -52,10 +54,23 @@ def i_have_the_value(step, value):
     assert world.value == value, \
         "Got value %s" % world.value
     
+@step(u'I have the json value (.*)')
+def i_have_the_json_value(step, value):
+    assert json.loads(value) == world.value, \
+        "Got value %s" % world.value
+    
 @step(u'I set settings (\S+) to (.*)')
 def i_set_settings_property_to_value(step, property, value):
     attr = None
     setattr(world.settings, property, value)
+    attr = getattr(world.settings, property)
+    assert attr is not None, \
+        "Got attr %s" % str(attr)
+    
+@step(u'I set json settings (\S+) to (.*)')
+def i_set_json_settings_property_to_value(step, property, value):
+    attr = None
+    setattr(world.settings, property, json.loads(str(value)))
     attr = getattr(world.settings, property)
     assert attr is not None, \
         "Got attr %s" % str(attr)
@@ -65,3 +80,45 @@ def i_reload_settings(step):
     reload(world.settings)
     assert world.settings is not None, \
         "Got settings %s" % world.settings
+    
+@step(u'I reload XML generation libraries')
+def i_reload_xml_generation_libraries(step):
+    reload(parseCSVFiles)
+    assert world.settings is not None, \
+        "Got settings %s" % world.settings
+    
+@step(u'I have article_id (\S+)')
+def i_have_article_id(step, article_id):
+    world.article_id = article_id
+    assert world.article_id is not None, \
+        "Got article_id %s" % world.article_id
+    
+@step('I have author_id (.*)')
+def i_have_author_id(step, author_id):
+    world.author_id = author_id
+    assert world.author_id is not None, \
+        "Got author_id %s" % world.author_id
+    
+@step(u'I set attribute to parseCSVFiles method (\S+)')
+def i_set_attribute_to_parsecsvfiles_method(step, method_name):
+    method = getattr(parseCSVFiles, method_name)
+    if world.author_id != "":
+        world.attribute = method(str(int(world.article_id)), str(int(world.author_id)))
+    else:
+        world.attribute = method(str(int(world.article_id)))
+        
+    assert world.attribute is not None, \
+        "Got attribute %s" % method_name + world.article_id
+   
+@step(u'I have attribute (.*)')
+def i_have_attribute_attribute(step, attribute):
+    # Type convert the value as required
+    if type(world.attribute) == str:
+        attribute = str(attribute)
+    elif type(world.attribute) == int:
+        attribute = int(attribute)
+    elif type(world.attribute) == list:
+        attribute = attribute.split(",")
+    # Compare value
+    assert world.attribute == attribute, \
+        "Got attribute %s" % world.attribute
