7ffd30d5bd2f7e1f3482650759708c0ab0d622f4
Luke Skibinski <l.skibinski@elifesciences.org>
2015-03-20 16:00:57 +0000

    added logging and error checking

--- decapitatePDF2.py
+++ decapitatePDF2.py
@@ -1,25 +1,30 @@
 import logging
 import subprocess
 
-logger = logging.getLogger("decapitator2")
-hdlr = logging.FileHandler("decapitator2.log")
-formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.setLevel(logging.INFO)
+FORMAT = logging.Formatter("%(created)f - %(levelname)s - %(processName)s - %(name)s - %(message)s")
+LOGFILE = "decapitator2.log"
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+
+h = logging.FileHandler(LOGFILE)
+h.setLevel(logging.INFO)
+h.setFormatter(FORMAT)
+
+logger.addHandler(h)
 
 def decapitate_pdf_with_error_check(pdf_in, pdf_out):
     f = subprocess.Popen(["/opt/strip-coverletter/strip-coverletter.sh", pdf_in, pdf_out], \
                          stdout=subprocess.PIPE, \
                          stderr=subprocess.PIPE)
+
     stderr = f.stderr.read()
     stdout = f.stdout.read()
+    
+    map(logger.info, stdout.splitlines())
+    map(logger.error, stderr.splitlines())
 
-    print 'stderr'
-    print stderr
-    print
-    print 'stdout'
-    print stdout
+    return stderr != '' # no errors
 
 if __name__ == '__main__':
     import sys
